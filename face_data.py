534dd9be5b7b4739b4fea2a110aa6d3d860eb4e4
azhongwl <azhong@AF051506.employee-wifi.rwc.corp.imvu.com>
2018-05-07 15:01:19 -0700

    init with data preparation for mouth and eye patches

---
+++ face_data.py
@@ -0,0 +1,45 @@
+import numpy as np
+
+class FaceLandmark:
+    def __init__(self, image, landmarks, landmark_type):
+        self.image = image
+        self.rows = image.shape[0]
+        self.cols = image.shape[1]
+        if len(image.shape) < 3:
+            self.channels = 1
+        else:
+            self.channels = image.shape[2]
+        self.landmarks = np.array(landmarks)
+        self.landmarks_homogenous = np.insert(self.landmarks, 2, 1.0, axis = 1)
+        self.landmarks_index_dict = {}
+        self.landmark_type = landmark_type
+        if landmark_type  == 'muct_clmtools':
+            self.landmarks_index_dict['face'] = range(71)
+            self.landmarks_index_dict['left_eye']  = [23, 24, 25, 26, 63, 64, 65, 66]
+            self.landmarks_index_dict['right_eye'] = [28, 29, 30, 31, 67, 68, 69, 70]
+            self.landmarks_index_dict['mouth'] = range(44, 62)
+        else:
+            assert False, '{} not supported'.format(landmark_type)
+        return
+
+    def get_bounding_box(self, part = 'face'):
+        x2, y2 = np.amax(self.landmarks[self.landmarks_index_dict[part]], axis = 0)
+        x1, y1 = np.amin(self.landmarks[self.landmarks_index_dict[part]], axis = 0)
+        x1 = min(int(x1 + 0.5), self.cols - 1)
+        x2 = min(int(x2 + 0.5), self.cols - 1)
+        # extend further to forehead
+        if part == 'face':
+            y1 = max(int(y1 - (y2 - y1) * 0.2 + 0.5), 0)
+        else:
+            y1 = min(int(y1 + 0.5), self.rows - 1)
+        y2 = min(int(y2 + 0.5), self.rows - 1)
+        return [x1, y1, x2, y2]
+
+class FaceData:
+    def __init__(self):
+        self.items = []
+        self.X = []
+        self.y = {}
+
+    def append(self, face_landmark):
+        assert isinstance(face_landmark, FaceLandmark)
---
+++ prepare_data.py
@@ -0,0 +1,99 @@
+import os
+import cv2
+import numpy as np
+
+from face_data import FaceData, FaceLandmark
+def prepare_data(data_path, annotation_path, landmark_type):
+    face_landmark_list = []
+    if landmark_type == 'muct_clmtools':
+        landmark_size = 71
+        with open(annotation_path) as fi:
+            for line in fi:
+                splitted = line.split(';')
+                filename = splitted[0]
+                filepath = os.path.join(data_path, filename)
+                if not os.path.isfile(filepath):
+                    continue
+                landmarks = np.array([[float(splitted[i*3+1]), float(splitted[i*3+2])] for i in range(landmark_size)])
+                image = cv2.cvtColor(cv2.imread(filepath), cv2.COLOR_BGR2GRAY)
+                face_landmark = FaceLandmark(image, landmarks, landmark_type)
+                face_landmark_list.append(face_landmark)
+            return face_landmark_list
+
+def augment_data(face_landmarks,
+                 augment_size = 10,
+                 rotate_bounding_box_part = 'face',    # rotate around part bounding box
+                 rotate_limit_in_degrees = 10,         # rotate limit defaults to -10 to 10 degrees
+                 scale_bounding_box_part = 'face',     # scale to fit
+                 scale_bounding_box_size = 64,         # scale the larger dimension to fit this
+                 scale_limit_ratio = 0.1,              # scale defaults from 0.9 to 1.1
+                 translate_bounding_box_part = 'face', # translate base
+                 translate_x_ratio = 0.1,              # translate ratio of part bounding box in x direction
+                 translate_y_ratio = 0.1,              # translate ratio of part bounding box in y direction
+                 output_bounding_box_part = 'face',    # center of output
+                 output_size = (64, 64),
+):
+    import random
+    random.seed()
+    output_face_landmarks = []
+    assert output_size[0] % 2 == 0 and output_size[1] % 2 ==0, 'output size needs to be even'
+    for fl in face_landmarks:
+        x1, y1, x2, y2 = fl.get_bounding_box(rotate_bounding_box_part)
+        rotate_center = ((x1 + x2) / 2.0, (y1 + y2) / 2.0)
+        x1, y1, x2, y2 = fl.get_bounding_box(scale_bounding_box_part)
+        scale_ratio = min(scale_bounding_box_size/1.0/(y2-y1),
+                          scale_bounding_box_size/1.0/(x2-x1))
+        x1, y1, x2, y2 = fl.get_bounding_box(translate_bounding_box_part)
+        translate_x = x2 - x1
+        translate_y = y2 - y1
+        x1, y1, x2, y2 = fl.get_bounding_box(output_bounding_box_part)
+        output_center = (int((x1+x2)/2.0), int((y1+y2)/2.0))
+        half_output_dim_x = int(output_size[0] / 2)
+        half_output_dim_y = int(output_size[1] / 2)
+        for augment_i in range(augment_size):
+            # randomly rotate image by -10 to 10 degrees
+            angle = (random.random()*2 - 1) * rotate_limit_in_degrees
+            # randomly scale image 0.9 - 1.1
+            ratio = ((random.random()*2-1) * scale_limit_ratio + 1) * scale_ratio
+            M = cv2.getRotationMatrix2D(rotate_center, angle, ratio)
+            image = cv2.warpAffine(fl.image, M, (fl.cols, fl.rows))
+            landmarks = np.transpose(np.dot(M, np.transpose(fl.landmarks_homogenous)))
+            # randomly translate image by ratio
+            translate_x = int((random.random()*2-1) * translate_x_ratio * output_size[0] + 0.5)
+            translate_y = int((random.random()*2-1) * translate_y_ratio * output_size[1] + 0.5)
+            translate_x = max(min(image.shape[1], translate_x), 0)
+            translate_y = max(min(image.shape[0], translate_y), 0)
+            image = image[output_center[1] - half_output_dim_y + translate_y: output_center[1] + half_output_dim_y + translate_y,
+                          output_center[0] - half_output_dim_x + translate_x: output_center[0] + half_output_dim_x + translate_x]
+            landmarks = (landmarks - np.array([output_center[0] - half_output_dim_x + translate_x,
+                                               output_center[1] - half_output_dim_y + translate_y]))
+            output_face_landmarks.append(FaceLandmark(image, landmarks, fl.landmark_type))
+        return output_face_landmarks
+
+
+# load muct data through https://github.com/azhongwl/clmtools
+face_landmarks = prepare_data('/Users/azhong/face/clmtools/pdm_builder/data/images/',
+                              '/Users/azhong/face/clmtools/pdm_builder/data/annotations.csv',
+                              'muct_clmtools')
+mouth_landmarks_augmented = augment_data(face_landmarks,
+                                         10,
+                                         'mouth', 10,
+                                         'face', 128, 0.1,
+                                         'mouth', 0.2, 0.2,
+                                         'mouth', (64, 64))
+left_eye_landmarks_augmented = augment_data(face_landmarks,
+                                            10,
+                                            'left_eye', 10,
+                                            'face', 128, 0.1,
+                                            'left_eye', 0.2, 0.2,
+                                            'left_eye', (32, 32))
+right_eye_landmarks_augmented = augment_data(face_landmarks,
+                                             10,
+                                             'right_eye', 10,
+                                             'face', 128, 0.1,
+                                             'right_eye', 0.2, 0.2,
+                                             'right_eye', (32, 32))
+import pickle
+pickle.dump(mouth_landmarks_augmented, open('data/mouth.p', 'wb'))
+pickle.dump(left_eye_landmarks_augmented, open('data/leye.p', 'wb'))
+pickle.dump(right_eye_landmarks_augmented, open('data/reye.p', 'wb'))
